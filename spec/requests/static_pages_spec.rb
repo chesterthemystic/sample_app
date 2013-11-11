11a3bf46b25648f174fd0261a18d70daa59cbc93
Chance Miller <ch@ncemiller.com>
2013-11-10 21:53:57 -0500

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,6 +3,18 @@ require_relative '../spec_helper.rb'
 describe "Static Pages" do
 	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
+	describe "Contact page" do
+		it "should have the content 'Contact'" do
+			visit '/static_pages/contact'
+			expect(page).to have_content('Contact')
+		end
+
+		it "should have the title 'Contact'" do
+			visit '/static_pages/contact'
+			expect(page).to have_title("#{base_title} | Contact")
+		end
+	end
+
 	describe "Home page" do
 		it "should have the content 'Sample App'" do
 			visit '/static_pages/home'
